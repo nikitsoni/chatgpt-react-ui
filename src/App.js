c9aeae048c15faac4426a517a30de7821a57d871
Nikit Soni <nikitsoni@Nikits-MacBook-Air.local>
2023-01-31 18:14:30 +0530

    added files and folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Article, Brand, CTA, Navbar } from './components/index.js'
+import {Blog, Features, Footer, Header, Possibility, whatGPT3} from './containers/index.js'
+
+const App = () => {
+  return (
+    <div>
+        <h1>Chat GPT</h1>
+    </div>
+  )
+}
+
+export default App
