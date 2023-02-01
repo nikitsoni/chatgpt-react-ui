0123e33cf58def91202867d64eb55b8debad6bed
Nikit Soni <nikitsoni@Nikits-MacBook-Air.local>
2023-02-01 18:12:49 +0530

    Added footer and tested for bugs

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({imgUrl, date, title}) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container__article'>
+      <div className='gpt3__blog-container__article-image'>
+        <img src={imgUrl} alt='blog'/>
+      </div>
+      <div className='gpt3__blog-container__article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
